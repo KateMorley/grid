b691302dc701040537a8f12e1bbe5c92955ae2c8
Kate Morley <34718897+KateMorley@users.noreply.github.com>
2024-10-02 13:24:19 +0100

    Add support for demand API's new date format

--- classes/Data/Time.php
+++ classes/Data/Time.php
@@ -1,13 +1,26 @@
 <?php
 
-// Functions for handling times
-
 namespace KateMorley\Grid\Data;
 
+/** Functions for handling times. */
 class Time {
+  private const MONTHS = [
+    'JAN' => 1,
+    'FEB' => 2,
+    'MAR' => 3,
+    'APR' => 4,
+    'MAY' => 5,
+    'JUN' => 6,
+    'JUL' => 7,
+    'AUG' => 8,
+    'SEP' => 9,
+    'OCT' => 10,
+    'NOV' => 11,
+    'DEC' => 12
+  ];
 
   /**
-   * Normalises a time and returns it as a "YYYY-MM-DD HH:MM:SS" string
+   * Normalises a time and returns it as a "YYYY-MM-DD HH:MM:SS" string.
    *
    * @param string $time     The time
    * @param int    $interval The time interval, in minutes
@@ -15,7 +28,6 @@ class Time {
    * @throws DataException If the time is invalid
    */
   public static function normalise(string $time, int $interval): string {
-
     if (!preg_match(
       '/^(\d\d\d\d)-(\d\d)-(\d\d)(T| )(2[0-3]|[01]\d):([0-5]\d)(:00)?Z?$/',
       $time,
@@ -35,7 +47,6 @@ class Time {
     }
 
     return '"' . str_replace(['T', 'Z'], [' ', ''], $time) . '"';
-
   }
 
   /**
@@ -53,12 +64,19 @@ class Time {
    * @throws DataException If the date or period is invalid
    */
   public static function getSettlementTime(string $date, int $period): string {
-
-    if (!preg_match('/^(\d\d\d\d)-(\d\d)-(\d\d)$/', $date, $matches)) {
+    if (preg_match('/^(\d\d\d\d)-(\d\d)-(\d\d)$/', $date, $matches)) {
+      $year = (int)$matches[1];
+      $month = (int)$matches[2];
+      $day = (int)$matches[3];
+    } elseif (preg_match('/^(\d\d)-([A-Z]{3})-(\d\d\d\d)$/', $date, $matches)) {
+      $day = (int)$matches[1];
+      $month = self::MONTHS[$matches[2]] ?? 0;
+      $year = (int)$matches[3];
+    } else {
       throw new DataException('Invalid settlement date format: ' . $date);
     }
 
-    if (!checkdate((int)$matches[2], (int)$matches[3], (int)$matches[1])) {
+    if (!checkdate($month, $day, $year)) {
       throw new DataException('Invalid settlement date: ' . $date);
     }
 
@@ -68,12 +86,7 @@ class Time {
 
     return gmdate(
       '"Y-m-d H:i:s"',
-      (
-        mktime(0, 0, 0, (int)$matches[2], (int)$matches[3], (int)$matches[1])
-        + ($period - 1) * 1800
-      )
+      mktime(0, 0, 0, $month, $day, $year) + ($period - 1) * 1800
     );
-
   }
-
 }
